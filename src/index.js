b8b52e12fed2ee5c4dbbf73cb4108a34a6874abe
baoolam28 <duongbaolam2214@gmail.com>
2024-07-11 12:32:09 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import ShopContent from './components/shop/shop.js';
-import Admin from './components/admin/admin.js';
-import AddProduct from './components/admin/addProduct.js';
-import EditProduct from './components/admin/editProduct.js';
-function App () {
-  return (
-    <>
-      {/* <Navbar />
-      <ShopContent /> */}
-    </>
-  );
-}
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
     <App />
     <Routes>
       <Route path="/" element={<ShopContent />} />
@@ -26,5 +16,12 @@ root.render(
       <Route path="/admin/edit-product/:id" element={<EditProduct />} />
     </Routes>
   </BrowserRouter>
+
+  </React.StrictMode>
+
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
